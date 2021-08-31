3f3715c28e471a19cabba8a03a122e311374bd7d
Giulia Chiola <giulia.chiola.17@gmail.com>
2021-08-31 17:40:04 +0200

    feat: add pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import PokemonList from './PokemonList';
+import Pagination from './Pagination';
 import axios from 'axios'
 
 /**
@@ -23,21 +24,43 @@ function App() {
 
     let cancel
     axios.get(currentPageUrl, {
+      /**
+       * Axios accepts an object of options as second parameter. We have to use `cancelToken` https://github.com/axios/axios#cancellation
+       * Every time we do a request, axios assign the `cancel` variable to the new CancelToken
+       */
       calcelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
       setLoading(false)
-      setPrevPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
+      setNextPageUrl(res.data.next)
       setPokemon(res.data.results.map(p => p.name))
     })
 
-    return () => cancel()
+    /**
+     * If the user calls the `useEffect` multiple times in a row before the `useEffect` is finished, there will be race condition that will override each other.
+     * To make sure to cancel every old request every time we make a new request, we have to return a function inside our `useEffect` and this function is called every single time the `useEffect` is called again. In our app, we want to clean the last API request and use just the new one.
+     */
+    return () => cancel() // cancel our request
   }, [currentPageUrl])
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
 
   if (isLoading) return 'Loading...'
 
   return (
-    <PokemonList pokemon={pokemon}/>
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination
+        goToPrevPage={prevPageUrl ? goToPrevPage : null}
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+      />
+    </>
   );
 }
 
